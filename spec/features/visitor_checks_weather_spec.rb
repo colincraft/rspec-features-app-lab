c86a65a10a89e15b6d83ebc1e0ee0c1d0407c35a
Colin Craft <colin@colincraft.com>
2015-03-04 14:53:27 -0800

    ¯\_(ツ)_/¯

---
+++ spec/features/visitor_checks_weather_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+feature 'Visitor checks the weather' do
+  scenario 'sunny' do
+    # setup
+    visit root_path
+
+
+    # exercise
+    fill_in "What's your zipcode", with: "94702"
+    click_button "Is it going rain?"
+    
+    # verify
+    expect(page).to have_content("Sun's gonna be shining!")
+    expect(page).to have_content("No umbrella needed.")
+
+
+  end
+end
